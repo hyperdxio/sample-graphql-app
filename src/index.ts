21f9ab37e205bdd43211163ba10772fa64cbc8e1
Warren <5959690+wrn14897@users.noreply.github.com>
2024-03-26 14:36:41 -0700

    feat: add winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import winston from 'winston';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { getWinstonTransport } from '@hyperdx/node-opentelemetry';
+
+
+const MAX_LEVEL = 'info';
+ 
+const logger = winston.createLogger({
+  level: MAX_LEVEL,
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    getWinstonTransport(MAX_LEVEL), // append this to the existing transports
+  ],
+});
+
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -36,7 +51,10 @@ const books = [
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: () => {
+      logger.info('Fetching books...');
+      return books;
+    },
   },
 };
 
